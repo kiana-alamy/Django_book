a348f430f43b03b6e1353f34ce66a4fb3c42bfb4
kiana alamy <as.faraso.97@gmail.com>
2023-06-14 00:44:33 +0430

    html

--- mysite/book/models.py
+++ mysite/book/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class book(models.Model):
+    title= models.CharField(max_length=100)
+    auther= models.CharField(max_length=150)
+    date= models.DateField
+    pagese= models.IntegerField
+    template= models.ImageField
